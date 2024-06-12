ca3ed79b1ca1241ff7748d6f618945a586c03224
Kemboiobed <162710603+Kemboiobed@users.noreply.github.com>
2024-06-12 20:33:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,18 +7,18 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['23470505241', 'EXCEL', true], 
-  ['234802696', 'EXCEL', true], 
-  ['070435241', '⚡Excel', true],
-  ['2347035241', 'Excel', true] ]
+  ['254740873466', 'EXCEL', true], 
+  ['254740873466', 'EXCEL', true], 
+  ['0740873466', '⚡Excel', true],
+  ['234740873466', 'Excel', true] ]
 
  //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2341', '2348021779696', '2347045035241','233533763772'] 
-global.prems = ['2347045035241','233208179431','233533763772']
-global.allowed = ['2348021779696', '2347045035241','233533763772']
+global.mods = ['2341', '234740873466', '234740873466','234740873466'] 
+global.prems = ['234740873466','234740873466','234740873466']
+global.allowed = ['234740873466', '234740873466','234740873466']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
